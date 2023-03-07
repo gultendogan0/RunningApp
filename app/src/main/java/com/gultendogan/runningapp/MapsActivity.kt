8e511e8f6b676a2843c31b091c2c606cae5c0629
gulten27 <ggdogann00@gmail.com>
2023-03-07 13:02:59 +0300

    Handling the Map's Lifecycle

---
+++ app/src/main/java/com/gultendogan/runningapp/MapsActivity.kt
@@ -0,0 +1,22 @@
+package com.gultendogan.runningapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
